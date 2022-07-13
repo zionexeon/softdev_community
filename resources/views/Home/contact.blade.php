15e8b57bb72b350c086a888685c97b91a065e8ec
zionexeonch <zionexeon@gmail.com>
2022-07-13 13:26:25 +0700

    add code

--- resources/views/Home/contact.blade.php
+++ resources/views/Home/contact.blade.php
@@ -1,32 +1,46 @@
 @extends('layouts.template')
 @section('content')
-    <div class="container justify-content-center align-items-center">
-        <div id="judul" class="mt-5">
-            <h1>Kontak</h1>
+<div class="container justify-content-center align-items-center">
+    <div id="judul" class="mt-5">
+        <h1>Kontak</h1>
+    </div>
+    <div class="row row-cols-1 row-cols-md-3 mt-5">
+        <div class="col-md-4 mb-3 pb-5" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100"
+            data-aos-once="false">
+            <a href="" class="nav-link">
+                <div class="card kartu-kontak">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-envelope fa-5x pt-3 pb-3"></i>
+                        <h4 class="fw-bold">Email</h4>
+                        <h5 class="card-text">example@gmail.com</h5>
+                    </div>
+                </div>
+            </a>
         </div>
-        <div class="row mt-5 justify-content-center text-center">
-            <div class="col-md-4">
-                <a href="mailto:ardianto8306@gmail.com" class="text-dark text-decoration-none">
-                    <i class="fa-solid fa-envelope fa-4x"></i>
-                    <h4 class="text-center mt-2">Email</h4>
-                    <h5 class="text-center mt-4 mb-5">example@gmail.com</h5>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="telp:088888901970" class="text-dark text-decoration-none">
-                    <i class="fa-solid fa-phone fa-4x"></i>
-                    <h4 class="text-center mt-2">Phone & WA</h4>
-                    <h5 class="text-center mt-4 mb-5">+62 XXX-XXXX-XXXX</h5>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="https://goo.gl/maps/2jBDUPrU6AGCWgVC6" target="_blank" class="text-dark text-decoration-none">
-                    <i class="fa-solid fa-map-location-dot fa-4x"></i>
-                    <h4 class="text-center mt-2">Maps</h4>
-                    <h5 class="text-center mt-4">Mekar Laser <span class="badge bg-dark text-white">Cutting Digital</span>
-                    </h5>
-                </a>
-            </div>
+        <div class="col-md-4 mb-3 pb-5" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="150"
+            data-aos-once="false">
+            <a href="" class="nav-link">
+                <div class="card kartu-kontak">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-phone fa-5x pt-3 pb-3"></i>
+                        <h4 class="fw-bold">Telepon</h4>
+                        <h5 class="card-text">+62XXX-XXXX-XXXX</h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4 mb-3 pb-5" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200"
+            data-aos-once="false">
+            <a href="" class="nav-link">
+                <div class="card kartu-kontak">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-map-location-dot fa-5x pt-3 pb-3"></i>
+                        <h4 class="fw-bold">Peta</h4>
+                        <h5 class="card-text">SoftDev Community</h5>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
+</div>
 @endsection
