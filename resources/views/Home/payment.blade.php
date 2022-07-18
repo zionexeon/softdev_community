280b85ccf28049bb70dc585a062a4577ae642f51
zionexeonch <zionexeon@gmail.com>
2022-07-18 10:53:31 +0700

    red

--- resources/views/Home/payment.blade.php
+++ resources/views/Home/payment.blade.php
@@ -1,13 +1,37 @@
 @extends("layouts.template")
 @section('content')
-<div id="judul" class="mt-5" data-aos="fade-down" data-aos-duration="1000">
+<div id="judul" class="mt-5 mb-3" data-aos="fade-down" data-aos-duration="1000">
     <h1>Pembayaran</h1>
 </div>
 <div class="container">
-    <div class="row row-cols-1 row-cols-md-3 g-4">
+    <article class="fs-5 mt-3" data-aos="fade-right" data-aos-duration="1000" data-aos-once="false"
+        data-aos-delay="100">
+        <p>Metode pembayaran bisa melalui :</p>
+    </article>
+    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center align-items-center">
         <div class="col">
-            <img src="/img/Mekar Laser Logo 2.png" alt="" style="width: 500px;" class="mb-5 mt-4" data-aos="fade-up"
-                data-aos-duration="1000" data-aos-once="false" data-aos-delay="100">
+            <img src="/img/bri.png" alt="" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
+                data-aos-once="false" data-aos-delay="150">
+        </div>
+        <div class="col">
+            <img src="/img/bni.png" alt="" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
+                data-aos-once="false" data-aos-delay="150">
+        </div>
+        <div class="col">
+            <img src="/img/bca.png" alt="" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
+                data-aos-once="false" data-aos-delay="150">
+        </div>
+        <div class="col">
+            <img src="/img/mandiri.png" alt="" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
+                data-aos-once="false" data-aos-delay="150">
+        </div>
+        <div class="col">
+            <img src="/img/dana.png" alt="" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
+                data-aos-once="false" data-aos-delay="150">
+        </div>
+        <div class="col">
+            <img src="/img/paypal.png" alt="" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
+                data-aos-once="false" data-aos-delay="150">
         </div>
     </div>
 </div>
