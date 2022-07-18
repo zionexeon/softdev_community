d1061247cf41499e9b70ed2ca8ff81154b98a71b
zionexeonch <zionexeon@gmail.com>
2022-07-18 11:09:05 +0700

    red

--- resources/views/Home/payment.blade.php
+++ resources/views/Home/payment.blade.php
@@ -8,30 +8,42 @@
         data-aos-delay="100">
         <p>Metode pembayaran bisa melalui :</p>
     </article>
-    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center align-items-center">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
         <div class="col">
-            <img src="/img/bri.png" alt="Bank BRI" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
-                data-aos-once="false" data-aos-delay="150">
+            <div class="card border-0">
+                <img src="/img/bri.png" alt="Bank BRI" data-aos="fade-up" data-aos-duration="1000" data-aos-once="false"
+                    data-aos-delay="150">
+            </div>
         </div>
         <div class="col">
-            <img src="/img/bni.png" alt="Bank BNI" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
-                data-aos-once="false" data-aos-delay="150">
+            <div class="card border-0">
+                <img src="/img/bca.png" alt="Bank BCA" data-aos="fade-up" data-aos-duration="1000" data-aos-once="false"
+                    data-aos-delay="150">
+            </div>
         </div>
         <div class="col">
-            <img src="/img/bca.png" alt="Bank BCA" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
-                data-aos-once="false" data-aos-delay="150">
+            <div class="card border-0">
+                <img src="/img/bni.png" alt="Bank BNI" data-aos="fade-up" data-aos-duration="1000" data-aos-once="false"
+                    data-aos-delay="150">
+            </div>
         </div>
         <div class="col">
-            <img src="/img/mandiri.png" alt="Bank MANDIRI" style="width: 300px;" data-aos="fade-up"
-                data-aos-duration="1000" data-aos-once="false" data-aos-delay="150">
+            <div class="card border-0">
+                <img src="/img/mandiri.png" alt="Bank MANDIRI" data-aos="fade-up" data-aos-duration="1000"
+                    data-aos-once="false" data-aos-delay="150">
+            </div>
         </div>
         <div class="col">
-            <img src="/img/dana.png" alt="DANA" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
-                data-aos-once="false" data-aos-delay="150">
+            <div class="card border-0">
+                <img src="/img/dana.png" alt="DANA" data-aos="fade-up" data-aos-duration="1000" data-aos-once="false"
+                    data-aos-delay="150">
+            </div>
         </div>
         <div class="col">
-            <img src="/img/paypal.png" alt="PAYPAL" style="width: 300px;" data-aos="fade-up" data-aos-duration="1000"
-                data-aos-once="false" data-aos-delay="150">
+            <div class="card border-0">
+                <img src="/img/paypal.png" alt="PAYPAL" data-aos="fade-up" data-aos-duration="1000"
+                    data-aos-once="false" data-aos-delay="150">
+            </div>
         </div>
     </div>
 </div>
